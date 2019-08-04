19b1da66647aa22e8beb035392f08a569235566a
Hannah Salameh <hannahsalameh18@gmail.com>
2019-08-04 14:45:40 -0700

    basic code

---
+++ main.py
@@ -0,0 +1,24 @@
+import webapp2
+import jinja2
+import os
+
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class HomePage(webapp2.RequestHandler):
+    def post(self):
+        home_template = the_jinja_env.get_template('templates/home.html')
+        self.response.write(home_template.render())
+
+class AboutMePage(webapp2.RequestHandler):
+    def post(self):
+        aboutme_template = the_jinja_env.get_template('templates/aboutme.html')
+        self.response.write(aboutme_template.render())
+
+app = webapp2.WSGIApplication([
+    ('/', HomePage),
+    ('/aboutme',AboutMePage),
+], debug=True)
